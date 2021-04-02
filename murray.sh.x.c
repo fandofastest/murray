5faddf9c18da3212e0cadbc4de81c541f6dee163
fando <fandofast@gmail.com>
2021-04-02 23:58:34 +0700

    f

---
+++ murray.sh.x.c
@@ -0,0 +1,656 @@
+#if 0
+	shc Version 4.0.2, Generic Shell Script Compiler
+	GNU GPL Version 3 Md Jahidul Hamid <jahidulhamid@yahoo.com>
+
+	shc -r -f murray.sh 
+#endif
+
+static  char data [] = 
+#define      shll_z	10
+#define      shll	((&data[0]))
+	"\160\150\014\303\040\213\167\143\163\123"
+#define      opts_z	1
+#define      opts	((&data[10]))
+	"\314"
+#define      chk2_z	19
+#define      chk2	((&data[11]))
+	"\340\115\351\011\222\301\002\351\217\037\202\127\375\040\310\315"
+	"\212\345\355"
+#define      tst2_z	19
+#define      tst2	((&data[34]))
+	"\267\374\034\251\006\247\335\040\055\104\232\037\177\047\120\343"
+	"\204\327\340\144\246\132\116\010\030"
+#define      rlax_z	1
+#define      rlax	((&data[55]))
+	"\260"
+#define      pswd_z	256
+#define      pswd	((&data[118]))
+	"\322\032\321\016\051\257\212\153\347\234\056\343\150\041\021\221"
+	"\377\002\041\267\377\075\141\010\126\026\264\256\013\254\036\336"
+	"\306\357\354\360\237\166\133\206\023\211\152\174\253\173\016\253"
+	"\175\057\143\174\155\304\204\303\333\070\161\347\345\220\145\226"
+	"\264\216\040\022\243\224\057\206\017\311\144\234\311\344\070\150"
+	"\360\305\023\361\025\040\355\140\145\322\070\244\043\236\073\330"
+	"\054\133\352\320\360\032\126\000\344\273\234\255\240\324\025\221"
+	"\232\050\202\257\111\160\017\257\102\110\123\146\346\217\077\023"
+	"\352\052\343\332\104\072\332\051\365\166\326\226\113\354\047\345"
+	"\024\251\225\136\032\245\015\135\355\141\304\323\360\003\346\333"
+	"\056\311\265\163\003\220\234\371\007\162\217\123\136\266\071\163"
+	"\140\316\322\173\163\340\330\141\102\234\064\062\240\033\016\317"
+	"\345\304\102\351\124\336\342\133\121\162\257\260\051\350\044\212"
+	"\266\367\005\052\327\336\213\031\172\300\114\033\334\132\352\301"
+	"\036\054\252\163\013\215\317\135\377\176\016\051\146\062\263\035"
+	"\051\270\110\001\226\324\033\021\224\147\055\161\302\027\062\341"
+	"\104\335\125\120\153\044\255\152\242\273\224\011\356\107\046\030"
+	"\000\157\031\227\103\064\250\330\234\325\111\137\355\174\100\061"
+	"\132\225\202\305\271\057\060\134\353\304\146\331\013\215\361\013"
+	"\374\013\243\077\100\114\027\334\042\141\073\017\335\174\305\253"
+	"\200\261\233\037\050\366\245\074\200\017\271\054\213\307\327\010"
+	"\367\073\205\144\000\012\050\333\102\231"
+#define      text_z	575
+#define      text	((&data[424]))
+	"\210\323\251\072\156\311\143\145\156\240\345\176\131\022\011\041"
+	"\352\022\031\045\227\175\222\325\005\225\034\122\361\264\116\332"
+	"\045\312\072\274\342\326\063\252\241\104\345\044\262\116\306\033"
+	"\210\341\265\343\165\012\035\047\354\242\024\347\135\140\142\245"
+	"\077\275\361\265\061\267\075\142\357\174\305\156\323\344\227\141"
+	"\345\276\037\122\007\061\364\233\247\176\172\255\012\223\345\320"
+	"\354\033\007\271\114\372\244\327\054\337\206\313\141\327\235\172"
+	"\142\316\220\066\153\250\044\001\036\221\306\124\370\310\173\322"
+	"\055\056\067\046\366\000\163\166\006\322\345\305\375\032\076\031"
+	"\232\016\076\214\127\333\371\331\240\212\345\370\206\027\360\302"
+	"\035\136\036\106\222\117\036\340\306\154\161\110\307\035\160\175"
+	"\306\273\237\271\013\344\343\003\103\256\355\053\024\011\111\225"
+	"\346\370\137\322\203\364\326\335\300\260\067\202\227\210\243\056"
+	"\340\031\143\001\166\072\216\106\145\272\237\112\327\123\214\031"
+	"\264\261\052\335\333\245\271\225\272\031\157\360\325\300\320\332"
+	"\260\273\277\011\315\362\206\370\155\144\334\350\366\076\272\162"
+	"\260\315\112\141\332\253\003\171\017\002\066\200\077\041\312\170"
+	"\146\165\326\220\133\146\363\274\346\144\132\100\206\174\231\007"
+	"\022\104\023\207\200\257\216\014\051\347\327\337\250\000\272\025"
+	"\267\332\316\010\171\234\065\264\336\056\353\243\370\357\337\113"
+	"\256\133\071\170\066\246\366\103\330\230\364\325\004\227\213\034"
+	"\372\326\166\072\105\204\365\135\120\327\126\026\151\224\323\126"
+	"\371\207\324\141\301\244\024\226\300\156\171\265\157\121\114\071"
+	"\102\177\336\071\162\272\330\205\030\121\113\340\304\275\345\254"
+	"\063\207\131\262\374\277\061\173\114\363\020\230\350\135\057\274"
+	"\372\001\014\071\356\351\170\305\175\022\220\136\143\324\366\237"
+	"\324\374\370\070\320\124\365\121\224\360\315\303\173\155\146\221"
+	"\322\055\247\301\210\003\321\026\347\146\246\052\353\020\227\007"
+	"\374\325\101\033\233\302\221\237\133\202\327\132\172\131\043\157"
+	"\177\000\153\211\170\061\211\357\140\330\004\214\160\074\347\141"
+	"\213\360\252\033\122\161\341\032\133\344\002\120\137\366\303\245"
+	"\327\362\352\045\155\025\353\176\240\135\117\252\342\113\031\042"
+	"\242\147\256\032\004\351\061\067\307\176\367\237\236\172\035\165"
+	"\233\250\301\276\371\252\134\225\172\215\002\342\040\055\335\323"
+	"\230\206\062\327\140\014\146\070\247\037\313\245\220\315\154\061"
+	"\034\136\150\170\264\107\342\207\223\270\033\153\236\345\362\012"
+	"\007\324\107\067\004\153\254\166\227\113\267\005\224\324\331\356"
+	"\001\302\107\237\014\045\241\245\001\344\077\304\014\151\115\340"
+	"\023\210\116\334\353\264\113\213\232\311\345\254"
+#define      msg1_z	65
+#define      msg1	((&data[1022]))
+	"\340\354\065\027\071\031\242\215\173\110\066\120\103\333\214\301"
+	"\013\165\312\376\200\223\103\057\321\126\372\217\204\152\133\130"
+	"\016\365\367\317\272\350\253\131\257\012\117\277\354\270\376\256"
+	"\340\067\252\216\136\247\321\237\253\362\356\206\146\201\001\241"
+	"\071\346\037\274\175\235\342\037\103\343"
+#define      date_z	1
+#define      date	((&data[1096]))
+	"\153"
+#define      inlo_z	3
+#define      inlo	((&data[1097]))
+	"\374\311\151"
+#define      xecc_z	15
+#define      xecc	((&data[1103]))
+	"\100\333\152\200\072\011\065\027\227\270\333\023\045\060\152\362"
+	"\014\045\364"
+#define      lsto_z	1
+#define      lsto	((&data[1119]))
+	"\206"
+#define      tst1_z	22
+#define      tst1	((&data[1125]))
+	"\303\341\321\251\000\212\374\220\172\370\220\252\125\036\032\017"
+	"\316\116\351\342\113\032\047\241\044\053\351\215"
+#define      chk1_z	22
+#define      chk1	((&data[1151]))
+	"\107\341\123\353\000\370\253\341\365\236\142\202\054\351\141\155"
+	"\063\370\317\026\115\271\023\111\350\114\143"
+#define      msg2_z	19
+#define      msg2	((&data[1176]))
+	"\362\344\275\124\306\253\134\121\145\302\326\130\160\271\021\051"
+	"\341\144\345\274\117"/* End of data[] */;
+#define      hide_z	4096
+#define SETUID 0	/* Define as 1 to call setuid(0) at start of script */
+#define DEBUGEXEC	0	/* Define as 1 to debug execvp calls */
+#define TRACEABLE	1	/* Define as 1 to enable ptrace the executable */
+#define HARDENING	0	/* Define as 1 to disable ptrace/dump the executable */
+#define HARDENINGSP	0	/* Define as 1 to disable bash child process */
+#define BUSYBOXON	0	/* Define as 1 to enable work with busybox */
+
+/* rtc.c */
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+/* 'Alleged RC4' */
+
+static unsigned char stte[256], indx, jndx, kndx;
+
+/*
+ * Reset arc4 stte. 
+ */
+void stte_0(void)
+{
+	indx = jndx = kndx = 0;
+	do {
+		stte[indx] = indx;
+	} while (++indx);
+}
+
+/*
+ * Set key. Can be used more than once. 
+ */
+void key(void * str, int len)
+{
+	unsigned char tmp, * ptr = (unsigned char *)str;
+	while (len > 0) {
+		do {
+			tmp = stte[indx];
+			kndx += tmp;
+			kndx += ptr[(int)indx % len];
+			stte[indx] = stte[kndx];
+			stte[kndx] = tmp;
+		} while (++indx);
+		ptr += 256;
+		len -= 256;
+	}
+}
+
+/*
+ * Crypt data. 
+ */
+void arc4(void * str, int len)
+{
+	unsigned char tmp, * ptr = (unsigned char *)str;
+	while (len > 0) {
+		indx++;
+		tmp = stte[indx];
+		jndx += tmp;
+		stte[indx] = stte[jndx];
+		stte[jndx] = tmp;
+		tmp += stte[indx];
+		*ptr ^= stte[tmp];
+		ptr++;
+		len--;
+	}
+}
+
+/* End of ARC4 */
+
+#if HARDENING
+
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/prctl.h>
+#define PR_SET_PTRACER 0x59616d61
+
+/* Seccomp Sandboxing Init */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/prctl.h>
+#include <sys/syscall.h>
+#include <sys/socket.h>
+
+#include <linux/filter.h>
+#include <linux/seccomp.h>
+#include <linux/audit.h>
+
+#define ArchField offsetof(struct seccomp_data, arch)
+
+#define Allow(syscall) \
+    BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, SYS_##syscall, 0, 1), \
+    BPF_STMT(BPF_RET+BPF_K, SECCOMP_RET_ALLOW)
+
+struct sock_filter filter[] = {
+    /* validate arch */
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, ArchField),
+    BPF_JUMP( BPF_JMP+BPF_JEQ+BPF_K, AUDIT_ARCH_X86_64, 1, 0),
+    BPF_STMT(BPF_RET+BPF_K, SECCOMP_RET_KILL),
+
+    /* load syscall */
+    BPF_STMT(BPF_LD+BPF_W+BPF_ABS, offsetof(struct seccomp_data, nr)),
+
+    /* list of allowed syscalls */
+    Allow(exit_group),  /* exits a process */
+    Allow(brk),         /* for malloc(), inside libc */
+    Allow(mmap),        /* also for malloc() */
+    Allow(munmap),      /* for free(), inside libc */
+
+    /* and if we don't match above, die */
+    BPF_STMT(BPF_RET+BPF_K, SECCOMP_RET_KILL),
+};
+struct sock_fprog filterprog = {
+    .len = sizeof(filter)/sizeof(filter[0]),
+    .filter = filter
+};
+
+/* Seccomp Sandboxing - Set up the restricted environment */
+void seccomp_hardening() {
+    if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
+        perror("Could not start seccomp:");
+        exit(1);
+    }
+    if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &filterprog) == -1) {
+        perror("Could not start seccomp:");
+        exit(1);
+    }
+} 
+/* End Seccomp Sandboxing Init */
+
+void arc4_hardrun(void * str, int len) {
+    //Decode locally
+    char tmp2[len];
+    memcpy(tmp2, str, len);
+
+	unsigned char tmp, * ptr = (unsigned char *)tmp2;
+
+    int lentmp = len;
+
+#if HARDENINGSP
+    //Start tracing to protect from dump & trace
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+        printf("Operation not permitted\n");
+        kill(getpid(), SIGKILL);
+        exit(1);
+    }
+
+    //Decode Bash
+    while (len > 0) {
+        indx++;
+        tmp = stte[indx];
+        jndx += tmp;
+        stte[indx] = stte[jndx];
+        stte[jndx] = tmp;
+        tmp += stte[indx];
+        *ptr ^= stte[tmp];
+        ptr++;
+        len--;
+    }
+
+    //Exec bash script
+    system(tmp2);
+
+    //Empty script variable
+    memcpy(tmp2, str, lentmp);
+
+    //Sinal to detach ptrace
+    ptrace(PTRACE_DETACH, 0, 0, 0);
+    exit(0);
+
+    /* Seccomp Sandboxing - Start */
+    seccomp_hardening();
+
+    exit(0);
+#endif /* HARDENINGSP Exit here anyway*/
+
+    int pid, status;
+    pid = fork();
+
+    if(pid==0) {
+
+        //Start tracing to protect from dump & trace
+        if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+            printf("Operation not permitted\n");
+            kill(getpid(), SIGKILL);
+            _exit(1);
+        }
+
+        //Decode Bash
+        while (len > 0) {
+            indx++;
+            tmp = stte[indx];
+            jndx += tmp;
+            stte[indx] = stte[jndx];
+            stte[jndx] = tmp;
+            tmp += stte[indx];
+            *ptr ^= stte[tmp];
+            ptr++;
+            len--;
+        }
+
+        //Exec bash script
+        system(tmp2);
+
+        //Empty script variable
+        memcpy(tmp2, str, lentmp);
+
+        //Sinal to detach ptrace
+        ptrace(PTRACE_DETACH, 0, 0, 0);
+        exit(0);
+    }
+    else {
+        wait(&status);
+    }
+
+    /* Seccomp Sandboxing - Start */
+    seccomp_hardening();
+
+    exit(0);
+} 
+#endif /* HARDENING */
+
+/*
+ * Key with file invariants. 
+ */
+int key_with_file(char * file)
+{
+	struct stat statf[1];
+	struct stat control[1];
+
+	if (stat(file, statf) < 0)
+		return -1;
+
+	/* Turn on stable fields */
+	memset(control, 0, sizeof(control));
+	control->st_ino = statf->st_ino;
+	control->st_dev = statf->st_dev;
+	control->st_rdev = statf->st_rdev;
+	control->st_uid = statf->st_uid;
+	control->st_gid = statf->st_gid;
+	control->st_size = statf->st_size;
+	control->st_mtime = statf->st_mtime;
+	control->st_ctime = statf->st_ctime;
+	key(control, sizeof(control));
+	return 0;
+}
+
+#if DEBUGEXEC
+void debugexec(char * sh11, int argc, char ** argv)
+{
+	int i;
+	fprintf(stderr, "shll=%s\n", sh11 ? sh11 : "<null>");
+	fprintf(stderr, "argc=%d\n", argc);
+	if (!argv) {
+		fprintf(stderr, "argv=<null>\n");
+	} else { 
+		for (i = 0; i <= argc ; i++)
+			fprintf(stderr, "argv[%d]=%.60s\n", i, argv[i] ? argv[i] : "<null>");
+	}
+}
+#endif /* DEBUGEXEC */
+
+void rmarg(char ** argv, char * arg)
+{
+	for (; argv && *argv && *argv != arg; argv++);
+	for (; argv && *argv; argv++)
+		*argv = argv[1];
+}
+
+void chkenv_end(void);
+
+int chkenv(int argc)
+{
+	char buff[512];
+	unsigned long mask, m;
+	int l, a, c;
+	char * string;
+	extern char ** environ;
+
+	mask = (unsigned long)getpid();
+	stte_0();
+	 key(&chkenv, (void*)&chkenv_end - (void*)&chkenv);
+	 key(&data, sizeof(data));
+	 key(&mask, sizeof(mask));
+	arc4(&mask, sizeof(mask));
+	sprintf(buff, "x%lx", mask);
+	string = getenv(buff);
+#if DEBUGEXEC
+	fprintf(stderr, "getenv(%s)=%s\n", buff, string ? string : "<null>");
+#endif
+	l = strlen(buff);
+	if (!string) {
+		/* 1st */
+		sprintf(&buff[l], "=%lu %d", mask, argc);
+		putenv(strdup(buff));
+		return 0;
+	}
+	c = sscanf(string, "%lu %d%c", &m, &a, buff);
+	if (c == 2 && m == mask) {
+		/* 3rd */
+		rmarg(environ, &string[-l - 1]);
+		return 1 + (argc - a);
+	}
+	return -1;
+}
+
+void chkenv_end(void){}
+
+#if HARDENING
+
+static void gets_process_name(const pid_t pid, char * name) {
+	char procfile[BUFSIZ];
+	sprintf(procfile, "/proc/%d/cmdline", pid);
+	FILE* f = fopen(procfile, "r");
+	if (f) {
+		size_t size;
+		size = fread(name, sizeof (char), sizeof (procfile), f);
+		if (size > 0) {
+			if ('\n' == name[size - 1])
+				name[size - 1] = '\0';
+		}
+		fclose(f);
+	}
+}
+
+void hardening() {
+    prctl(PR_SET_DUMPABLE, 0);
+    prctl(PR_SET_PTRACER, -1);
+
+    int pid = getppid();
+    char name[256] = {0};
+    gets_process_name(pid, name);
+
+    if (   (strcmp(name, "bash") != 0) 
+        && (strcmp(name, "/bin/bash") != 0) 
+        && (strcmp(name, "sh") != 0) 
+        && (strcmp(name, "/bin/sh") != 0) 
+        && (strcmp(name, "sudo") != 0) 
+        && (strcmp(name, "/bin/sudo") != 0) 
+        && (strcmp(name, "/usr/bin/sudo") != 0)
+        && (strcmp(name, "gksudo") != 0) 
+        && (strcmp(name, "/bin/gksudo") != 0) 
+        && (strcmp(name, "/usr/bin/gksudo") != 0) 
+        && (strcmp(name, "kdesu") != 0) 
+        && (strcmp(name, "/bin/kdesu") != 0) 
+        && (strcmp(name, "/usr/bin/kdesu") != 0) 
+       )
+    {
+        printf("Operation not permitted\n");
+        kill(getpid(), SIGKILL);
+        exit(1);
+    }
+}
+
+#endif /* HARDENING */
+
+#if !TRACEABLE
+
+#define _LINUX_SOURCE_COMPAT
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#if !defined(PT_ATTACHEXC) /* New replacement for PT_ATTACH */
+   #if !defined(PTRACE_ATTACH) && defined(PT_ATTACH)
+       #define PT_ATTACHEXC	PT_ATTACH
+   #elif defined(PTRACE_ATTACH)
+       #define PT_ATTACHEXC PTRACE_ATTACH
+   #endif
+#endif
+
+void untraceable(char * argv0)
+{
+	char proc[80];
+	int pid, mine;
+
+	switch(pid = fork()) {
+	case  0:
+		pid = getppid();
+		/* For problematic SunOS ptrace */
+#if defined(__FreeBSD__)
+		sprintf(proc, "/proc/%d/mem", (int)pid);
+#else
+		sprintf(proc, "/proc/%d/as",  (int)pid);
+#endif
+		close(0);
+		mine = !open(proc, O_RDWR|O_EXCL);
+		if (!mine && errno != EBUSY)
+			mine = !ptrace(PT_ATTACHEXC, pid, 0, 0);
+		if (mine) {
+			kill(pid, SIGCONT);
+		} else {
+			perror(argv0);
+			kill(pid, SIGKILL);
+		}
+		_exit(mine);
+	case -1:
+		break;
+	default:
+		if (pid == waitpid(pid, 0, 0))
+			return;
+	}
+	perror(argv0);
+	_exit(1);
+}
+#endif /* !TRACEABLE */
+
+char * xsh(int argc, char ** argv)
+{
+	char * scrpt;
+	int ret, i, j;
+	char ** varg;
+	char * me = argv[0];
+	if (me == NULL) { me = getenv("_"); }
+	if (me == 0) { fprintf(stderr, "E: neither argv[0] nor $_ works."); exit(1); }
+
+	ret = chkenv(argc);
+	stte_0();
+	 key(pswd, pswd_z);
+	arc4(msg1, msg1_z);
+	arc4(date, date_z);
+	if (date[0] && (atoll(date)<time(NULL)))
+		return msg1;
+	arc4(shll, shll_z);
+	arc4(inlo, inlo_z);
+	arc4(xecc, xecc_z);
+	arc4(lsto, lsto_z);
+	arc4(tst1, tst1_z);
+	 key(tst1, tst1_z);
+	arc4(chk1, chk1_z);
+	if ((chk1_z != tst1_z) || memcmp(tst1, chk1, tst1_z))
+		return tst1;
+	arc4(msg2, msg2_z);
+	if (ret < 0)
+		return msg2;
+	varg = (char **)calloc(argc + 10, sizeof(char *));
+	if (!varg)
+		return 0;
+	if (ret) {
+		arc4(rlax, rlax_z);
+		if (!rlax[0] && key_with_file(shll))
+			return shll;
+		arc4(opts, opts_z);
+#if HARDENING
+	    arc4_hardrun(text, text_z);
+	    exit(0);
+       /* Seccomp Sandboxing - Start */
+       seccomp_hardening();
+#endif
+		arc4(text, text_z);
+		arc4(tst2, tst2_z);
+		 key(tst2, tst2_z);
+		arc4(chk2, chk2_z);
+		if ((chk2_z != tst2_z) || memcmp(tst2, chk2, tst2_z))
+			return tst2;
+		/* Prepend hide_z spaces to script text to hide it. */
+		scrpt = malloc(hide_z + text_z);
+		if (!scrpt)
+			return 0;
+		memset(scrpt, (int) ' ', hide_z);
+		memcpy(&scrpt[hide_z], text, text_z);
+	} else {			/* Reexecute */
+		if (*xecc) {
+			scrpt = malloc(512);
+			if (!scrpt)
+				return 0;
+			sprintf(scrpt, xecc, me);
+		} else {
+			scrpt = me;
+		}
+	}
+	j = 0;
+#if BUSYBOXON
+	varg[j++] = "busybox";
+	varg[j++] = "sh";
+#else
+	varg[j++] = argv[0];		/* My own name at execution */
+#endif
+	if (ret && *opts)
+		varg[j++] = opts;	/* Options on 1st line of code */
+	if (*inlo)
+		varg[j++] = inlo;	/* Option introducing inline code */
+	varg[j++] = scrpt;		/* The script itself */
+	if (*lsto)
+		varg[j++] = lsto;	/* Option meaning last option */
+	i = (ret > 1) ? ret : 0;	/* Args numbering correction */
+	while (i < argc)
+		varg[j++] = argv[i++];	/* Main run-time arguments */
+	varg[j] = 0;			/* NULL terminated array */
+#if DEBUGEXEC
+	debugexec(shll, j, varg);
+#endif
+	execvp(shll, varg);
+	return shll;
+}
+
+int main(int argc, char ** argv)
+{
+#if SETUID
+   setuid(0);
+#endif
+#if DEBUGEXEC
+	debugexec("main", argc, argv);
+#endif
+#if HARDENING
+	hardening();
+#endif
+#if !TRACEABLE
+	untraceable(argv[0]);
+#endif
+	argv[1] = xsh(argc, argv);
+	fprintf(stderr, "%s%s%s: %s\n", argv[0],
+		errno ? ": " : "",
+		errno ? strerror(errno) : "",
+		argv[1] ? argv[1] : "<null>"
+	);
+	return 1;
+}
